ea19afbf94864f3b27b0cc2d191d0f8a1dd691cc
Tarun Giri <tarun.giri@imaginea.com>
2018-05-11 12:12:05 +0530

    prettier translation app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,6 @@ import { TranslateService } from "@ngx-translate/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "Translation app";
-
   user = {
     name: "Tarun",
     age: 27
